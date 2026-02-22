611f631c05db24f680f5fe9702f7475d0ff07b94
gabi810 <gabrielasilva96484@gmail.com>
2026-02-22 23:14:27 +0000

    cartas cadastradas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,54 +1,47 @@
 #include <stdio.h>
 
+struct carta{
+    char codigo[4];
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+};
+
 int main() {
-    printf("Desafio Super Trufo!\n");
-    printf("Novo commit\n");
+    struct carta cartas[32];
 
-    struct carta{
-        char codigo[4];
-        int populacao;
-        float  area;
-        float pib;
-        int pontos_turisticos;
+    for (int i = 0; i < 32; i++){
+        printf("cadastro da carta %d\n", i + 1);
 
-    };
-    int main () {
-        struct  carta cartas [32];
+        printf("codigo da carta: ");
+        scanf("%s", cartas[i].codigo);
 
-        for ((int i = 0; < 32; i++) ;{
-            printf("cadastro da carta %\n", i + 1);
+        printf("populacao: ");
+        scanf("%d", &cartas[i].populacao);
 
-            printf("codigo da carta:");
-            scanf("%s", cartas[i].codigo);
+        printf("area: ");
+        scanf("%f", &cartas[i].area);
 
-            printf("populacao: ");
-            scanf("%d", &cartas[i].populacao);
+        printf("pib: ");
+        scanf("%f", &cartas[i].pib);
 
-            printf("area: ");
-            scanf("%f", &cartas[i].area);
+        printf("pontos turisticos: ");
+        scanf("%d", &cartas[i].pontos_turisticos);
 
-            printf("pib: ");
-            scanf("%f", &cartas[i].pib);
-
-            printf("pontos turisticos: ");
-
-
-        }
-        
-        {
-            /* code */
-        }
-        
-        {
-            /* data */
-        };
-        
+        printf("\n");
     }
+    
+    printf("\n=== cartas cadastradas ===\n");
+    return 0;
+        /* code */
+    }
+    
     {
         /* data */
     };
     
-    
+}
 
-return 0;
-} 
+
+

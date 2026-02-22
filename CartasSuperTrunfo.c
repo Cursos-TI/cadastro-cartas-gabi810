52e8c07d811501c18f69e1ebbb943804fbbc6c33
gabi810 <gabrielasilva96484@gmail.com>
2026-02-22 20:19:54 +0000

    cadastrroda carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,5 +4,40 @@ int main() {
     printf("Desafio Super Trufo!\n");
     printf("Novo commit\n");
 
+    struct carta{
+        char codigo[4];
+        int populacao;
+        float  area;
+        float pib;
+        int pontos_turisticos;
+
+    };
+    int main () {
+        struct  carta cartas [32];
+
+        for ((int i = 0; < 32; i++) ;{
+            printf("cadastro da carta %\n", i + 1);
+
+            printf("codigo da carta:");
+            scanf("%s", cartas[i].codigo);
+
+            
+        }
+        
+        {
+            /* code */
+        }
+        
+        {
+            /* data */
+        };
+        
+    }
+    {
+        /* data */
+    };
+    
+    
+
 return 0;
 } 
